3f8b7715c420ee045f6fabdd3254596d9cad4039
robi <robert.szaloki@euedge.com>
2017-11-24 19:15:41 +0100

    write ssid and password to file

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include <BLEUtils.h>
 #include <BLEServer.h>
 #include <WiFi.h>
+#include <FS.h>
+#include "SPIFFS.h"
 
 // See the following for generating UUIDs:
 // https://www.uuidgenerator.net/
@@ -11,6 +13,7 @@
 #define SERVICE_UUID              "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define SSID_CHARACTERISTIC_UUID  "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 #define PWD_CHARACTERISTIC_UUID   "3e391f2f-e0da-41e1-b4d1-469aa606c490"
+#define WIFI_FILE                 "/wifi.txt"
 
 
 bool wifi_connected = false;
@@ -18,6 +21,36 @@ bool wifi_connecting = false;
 
 BLECharacteristic *pSSID_Characteristic;
 BLECharacteristic *pPWD_Characteristic;
+
+void readFile(fs::FS &fs, const char * path){
+    Serial.printf("Reading file: %s\n", path);
+
+    File file = fs.open(path);
+    if(!file || file.isDirectory()){
+        Serial.println("Failed to open file for reading");
+        return;
+    }
+
+    Serial.print("Read from file: ");
+    while(file.available()){
+        Serial.write(file.read());
+    }
+}
+
+void writeFile(fs::FS &fs, const char * path, const char * message){
+    Serial.printf("Writing file: %s\n", path);
+
+    File file = fs.open(path, FILE_WRITE);
+    if(!file){
+        Serial.println("Failed to open file for writing");
+        return;
+    }
+    if(file.print(message)){
+        Serial.println("File written");
+    } else {
+        Serial.println("Write failed");
+    }
+}
 
 void WiFiEvent(WiFiEvent_t event)
 {
@@ -30,6 +63,7 @@ void WiFiEvent(WiFiEvent_t event)
         Serial.println("IP address: ");
         Serial.println(WiFi.localIP());
         wifi_connected = true;
+        writeFile(SPIFFS, WIFI_FILE, pSSID_Characteristic->getValue().c_str());
         break;
     case SYSTEM_EVENT_STA_DISCONNECTED:
         if(wifi_connected) Serial.println("WiFi lost connection");
@@ -62,8 +96,15 @@ void setup() {
                                          BLECharacteristic::PROPERTY_WRITE
                                        );
 
-  pSSID_Characteristic->setValue("ssid");
-  pPWD_Characteristic->setValue("password");
+  if(!SPIFFS.begin()) {
+    Serial.println("SPIFFS Mount Failed");
+    pSSID_Characteristic->setValue("ssid");
+    pPWD_Characteristic->setValue("password");
+  } else {
+    readFile(SPIFFS, WIFI_FILE);
+    pSSID_Characteristic->setValue("ssid");
+    pPWD_Characteristic->setValue("password");
+  }
 
   pService->start();
 
@@ -74,7 +115,6 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   delay(1000);
   if(!wifi_connected && !wifi_connecting) {
     Serial.print(".");
